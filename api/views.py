b3dfefcde2f3d336f01238ef97d3c4c76610a52d
Newton Karanu <karanunewton4@gmail.com>
2021-07-31 12:27:42 +0300

    created viewset for points

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
 # Create your views here.
+from api.models import Points
+from api.serializers import PointsSerializer
+
+
+class PointsViewSets(viewsets.ModelViewSet):
+    """
+        API endpoint that allows points to be viewed
+    """
+    queryset = Points.objects.all()
+    serializer_class = PointsSerializer
