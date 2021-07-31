42c313684539b4e9412b80c6563d17bc766044e0
Newton Karanu <karanunewton4@gmail.com>
2021-07-31 12:08:07 +0300

    added closest_point to model and ran migrations -WIP

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from api.models import Points
+
 # Register your models here.
 
-admin.register(Points)
+admin.site.register(Points)
--- api/models.py
+++ api/models.py
@@ -1,7 +1,13 @@
+import uuid
 from django.db import models
 
 
 # Create your models here.
 class Points(models.Model):
-    string_of_csv = models.CharField(max_length=500)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    string_of_csv = models.TextField(max_length=500)
+    closest_point = models.CharField(max_length=100, default="(0,0)")
 
+
+# class FormattedPoints(models.Model):
+#     pass
