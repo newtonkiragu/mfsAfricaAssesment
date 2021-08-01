eabec43047ffb1697f3de4fc3ce12de902e51cb6
Newton Karanu <karanunewton4@gmail.com>
2021-08-01 13:47:18 +0300

    updated views to display points data

--- api/views.py
+++ api/views.py
@@ -1,13 +1,40 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-# Create your views here.
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from rest_framework import generics, status, viewsets
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+
 from api.models import Points
 from api.serializers import PointsSerializer
 
 
-class PointsViewSets(viewsets.ModelViewSet):
+class PointsViewSet(viewsets.ViewSet):
     """
-        API endpoint that allows points to be viewed
+    Points Viewset to display a list of points and the computed points.
+    Can perform GET and POST
     """
-    queryset = Points.objects.all()
-    serializer_class = PointsSerializer
+
+    def list(self, request):
+        queryset = Points.objects.all()
+        serializer = PointsSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        print(request.data)
+        points = list(eval(request.data))
+        result = Points.closest_points(points)
+        data = {"string_of_csv": str(points), "closest_point": str(result)}
+        print(data)
+        serializer = PointsSerializer(data=data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        pointsModel = Points(**serializer.data)
+        pointsModel.save()
+        return Response({'items': result})
+
+    def retrieve(self, request, pk=None):
+        queryset = Points.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = PointsSerializer(user)
+        return Response(serializer.data)
