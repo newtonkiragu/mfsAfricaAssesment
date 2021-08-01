1b92c1edddba80d6059b12485809949ecd4940f4
Newton Karanu <karanunewton4@gmail.com>
2021-08-01 13:48:51 +0300

    created tests. --WIP

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
-
 from api.models import Points
 
 
-class PointsSerializer(serializers.Serializer):
+class PointsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Points
         fields = ['string_of_csv', 'closest_point']
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from . import models
+
+
+class PointsTestCase(APITestCase):
+
+    def setUp(self):
+        self.new_set_of_points = models.Points(1, "(2,3), (1,1), (5, 4)")
+
+    def test_view_points(self):
+        old_count = models.Points.objects.count()
+        self.new_set_of_points.save()
+        new_count = models.Points.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
+    def test_view_point(self):
+        point_id = 1
+        self.new_set_of_points.save()
+        point = models.Points.objects.get(pk=point_id)
+        response = self.client.get(reverse('point', kwargs={'pk': point.id}), format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, point)
+
+    def test_update_point(self):
+        point_id = 1
+        self.new_set_of_points.save()
+        point = models.Points.objects.get(pk=point_id)
+        change_point = {'code': 'return False'}
+        res = self.client.put(
+            reverse('point', kwargs={'pk': point.id}),
+            change_point, format='json'
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_delete_point(self):
+        point_id = 1
+        # self.new_set_of_points.save()
+        point = models.Points.objects.get(pk=point_id)
+        response = self.client.delete(
+            reverse('point', kwargs={'pk': point.id}),
+            format='json',
+            follow=True)
+
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
