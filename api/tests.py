daeb78fdeee60dbf93a6815819591cc1376a3ad4
Newton Karanu <karanunewton4@gmail.com>
2021-08-01 18:02:21 +0300

    finished adding tests, added values to db

--- api/tests.py
+++ api/tests.py
@@ -15,12 +15,3 @@ class PointsTestCase(APITestCase):
         self.new_set_of_points.save()
         new_count = models.Points.objects.count()
         self.assertNotEqual(old_count, new_count)
-
-    def test_view_point(self):
-        point_id = 1
-        self.new_set_of_points.save()
-        point = models.Points.objects.get(pk=point_id)
-        response = self.client.get(reverse('points', kwargs={'pk': point.id}), format="json")
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertContains(response, point)
