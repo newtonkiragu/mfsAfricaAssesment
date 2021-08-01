ce5a32691e5536f44421b2287be622796d1e33e9
Newton Karanu <karanunewton4@gmail.com>
2021-08-01 13:45:06 +0300

    updated points model to compute closest points

--- api/models.py
+++ api/models.py
@@ -4,9 +4,15 @@ from django.db import models
 
 # Create your models here.
 class Points(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     string_of_csv = models.TextField(max_length=500)
     closest_point = models.CharField(max_length=100, default="(0,0)")
+
+    @staticmethod
+    def closest_points(points):
+        k = 2
+        points.sort(key=lambda K: K[0] ** 2 + K[1] ** 2)
+        return points[:k]
 
 
 # class FormattedPoints(models.Model):
